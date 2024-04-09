fffd962061d56a609a7a00613979379c0242854f
anaddict100@gmail.com <anaddict100@gmail.com>
2024-04-09 11:27:36 +0900

    modify contents

--- CppToyProjectServer/Threads/communicationThread.h
+++ CppToyProjectServer/Threads/communicationThread.h
@@ -3,25 +3,15 @@
 
 struct ThreadParameters
 {
-	bool isServerUser; 
+	bool bServerUser; 
 };
 
 DWORD WINAPI communicationThread(ThreadParameters threadParameters)
 {
-	if (threadParameters.isServerUser)
+	if (threadParameters.bServerUser)
 	{
-		SOCKET serverSocket = WSASocket(PF_INET, SOCK_STREAM, 0, NULL, 0, WSA_FLAG_OVERLAPPED);
-		u_long nonBlockingMode = 1;
-		ioctlsocket(serverSocket, FIONBIO, &nonBlockingMode); // Non Blocking listen socket
 
-		SOCKADDR_IN serverAddress;
-		ZeroMemory(&serverAddress, sizeof(serverAddress));
-		serverAddress.sin_family = AF_INET;
-		serverAddress.sin_addr.s_addr = htonl(INADDR_ANY);
-		serverAddress.sin_port = htons(1959); // Test 
 
-		bind(serverSocket, (SOCKADDR*)&serverAddress, sizeof(serverAddress));
-		listen(serverSocket, SOMAXCONN);
 	}
 
 	while (true)
