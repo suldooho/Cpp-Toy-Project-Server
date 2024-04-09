c0c1b78eee0fe81542a6898fed9aabf8f06c95ca
anaddict100@gmail.com <anaddict100@gmail.com>
2024-04-09 15:30:41 +0900

    modify contents

--- CppToyProjectServer/Threads/communicationThread.h
+++ CppToyProjectServer/Threads/communicationThread.h
@@ -1,19 +1,32 @@
 #pragma once
 #include "../Main/headers.h"
-
-struct ThreadParameters
+  
+std::vector<HANDLE> createThreades()
 {
-	bool bServerUser; 
-};
+	SYSTEM_INFO systemInfo;
+	GetSystemInfo(&systemInfo);
+	CommunicationManager& communicationManager = CommunicationManager::getInstance();
+	std::vector<HANDLE> handles;
 
-DWORD WINAPI communicationThread(ThreadParameters threadParameters)
-{
-	if (threadParameters.bServerUser)
+	for (int i = 0; i < systemInfo.dwNumberOfProcessors; ++i)
 	{
-
-
+		HANDLE handle = (HANDLE)_beginthreadex(NULL, 0, communicationThread, NULL, 0, NULL); // Test
+		handles.emplace_back(handle);
 	}
 
+	return handles;
+}
+
+void terminateThreades(std::vector<HANDLE> handles)
+{
+	for (auto& handle : handles)
+	{
+		CloseHandle(handle);
+	}  
+}
+
+unsigned __stdcall communicationThread(LPVOID param)
+{  
 	while (true)
 	{
 	}
